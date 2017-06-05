e1a9bc8d7ac44b1481b20f533782fa70e828f0c3
gaole <gaole@umich.edu>
2017-06-05 10:58:54 -0700

    some changes

--- main.py
+++ main.py
@@ -9,7 +9,64 @@ import os
 warnings.filterwarnings("ignore")
 
 
-# def uncertainty_sampling_bagofword(filena):
+def uncertainty_sampling_bagofword(filena):
+
+	def file_len(fname):
+	    with open(fname) as f:
+	        for i, l in enumerate(f):
+	            pass
+	    return i + 1
+
+	xlabel = [];
+	for i in range(9):
+		xlabel.append(float(i)/10.0 + 0.1);
+	ylabel = range(9);
+	for i in range(9):
+		ylabel[i] = 0.0;
+
+	random.seed(1);
+
+	filelen = file_len(filena);
+
+	shufflelist = range(filelen);
+
+	random.shuffle(shufflelist);
+
+	y = [];
+	corpus = [];
+
+	for i, line in enumerate(fp):
+		vec = line.split("\t")
+		if (whethershown == False):
+			truechar = vec[3][0];
+			whethershown = True;
+			y.append(1);
+		elif (whethershown == True):
+			if (truechar == vec[3][0]):
+				y.append(1);
+			else:
+				y.append(0);
+		corpus.append(vec[3]); 
+
+
+	corpus_train = [];
+	y_train = [];
+	for i in range(filelen / 10);
+		corpus_train.append(corpus[shufflelist[i]]);
+		y_train.append(y[shufflelist[i]]);
+
+	for i in range(filelen/10):
+		corpus.remove(corpus_train[i]);
+
+
+	X = bigram_vectorizer.fit_transform(corpus_train).toarray();
+	lr = linear_model.LogisticRegression()
+	lr.fit(X, y_train)
+	X = bigram_vectorizer.transform(testcorpus).toarray();
+
+
+
+	# for i in range()
 
 
 
